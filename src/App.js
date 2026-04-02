93b4325dec951cdecf668bdc127c4049d9205347
lafxindex <luther.adjeif@gmail.com>
2026-04-02 23:00:40 +0200

    Build trader directory

--- src/App.js
+++ src/App.js
@@ -4,12 +4,36 @@ import DashboardPage from "./pages/DashboardPage";
 import TradeDetailPage from "./pages/TradeDetailPage";
 import LoginPage from "./pages/LoginPage";
 import PublicTraderPage from "./pages/PublicTraderPage";
+import TraderDirectoryPage from "./pages/TraderDirectoryPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/" element={<TraderDirectoryPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/trades/:id" element={<TradeDetailPage />} />
+        <Route path="/trader/:slug" element={<PublicTraderPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import TradeDetailPage from "./pages/TradeDetailPage";
+import LoginPage from "./pages/LoginPage";
+import PublicTraderPage from "./pages/PublicTraderPage";
+import TraderDirectoryPage from "./pages/TraderDirectoryPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TraderDirectoryPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/trades/:id" element={<TradeDetailPage />} />
         <Route path="/trader/:slug" element={<PublicTraderPage />} />
